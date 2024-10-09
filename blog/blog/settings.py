68a1262ab329b11265d387920ca774788857bb7e
benjaminpaez <benjaapaez9@gmail.com>
2024-10-08 22:04:52 -0300

    add home page

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -1,5 +1,17 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
+
+# Rutas para archivos de medios
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Rutas para archivos estáticos
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 # Carga las variables del archivo .env
 load_dotenv()

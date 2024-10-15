a154b35de90f3e65b1ad5fd3f5a8f0105b071fb1
benjaminpaez <benjaapaez9@gmail.com>
2024-10-15 19:11:06 -0300

    Actualización de URLs y vistas en el módulo de post

--- blog/apps/post/views.py
+++ blog/apps/post/views.py
@@ -1,6 +1,14 @@
 from django.views.generic import TemplateView
 
 
+class PostListView(TemplateView):
+    template_name = 'post/post_list.html'
+
+
+class PostCreateView(TemplateView):
+    template_name = 'post/post_create.html'
+
+
 class PostDetailView(TemplateView):
     template_name = 'post/post_detail.html'
 

9c10917803a9de7ff4e20a9e32269e23cfc735cf
benjaminpaez <benjaapaez9@gmail.com>
2024-10-23 14:22:05 -0300

    agrego card index

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,15 +1,22 @@
 from django.views.generic import TemplateView
+from apps.post.models import Post
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_post'] = Post.objects.all().order_by('-creation_date')[:3]
+        return context
 
 
 class AboutView(TemplateView):
     template_name = 'sections/about.html'
 
+
 class ContactView(TemplateView):
     template_name = 'sections/contact.html'
+
 
 class AuthLoginView(TemplateView):
     template_name = 'auth/auth_login.html'
